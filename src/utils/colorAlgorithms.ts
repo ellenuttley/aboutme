a88299c5ba7c565ccd4a4869ff5372391d34be19
ellenuttley <ellenuttley@gmail.com>
2024-03-31 21:46:14 +0100

    black algorithm added

--- src/utils/colorAlgorithms.ts
+++ src/utils/colorAlgorithms.ts
@@ -5,7 +5,7 @@ type RGB = {
   green: number;
   blue: number;
   [key: string]: number;
-};
+ };
 
 export const hexToRgb = (hex: string): RGB => { 
     let red = parseInt(hex.slice(1, 3), 16),
@@ -15,14 +15,14 @@ export const hexToRgb = (hex: string): RGB => {
     return { red, green, blue };
  };
 
- export const rgbToHex = (r: number, g: number, b: number): string => { 
+export const rgbToHex = (r: number, g: number, b: number): string => { 
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  };
+ };
 
-  export const whiteAlgorithm = (rgb: RGB): string => { 
+export const whiteAlgorithm = (rgb: RGB): string => { 
     let lastDigits = [rgb.red, rgb.green, rgb.blue];
     for (let i = 0; i < lastDigits.length; i++) {
-      lastDigits[i] = (lastDigits[i] % 10);
+    lastDigits[i] = (lastDigits[i] % 10);
     }
     lastDigits.sort((a, b) => a - b);
 
@@ -31,8 +31,30 @@ export const hexToRgb = (hex: string): RGB => {
 
     const result: RGB = {red: 0, green: 0, blue: 0};
     for (let i = 0; i < colorMapping.length; i++) {
-      result[colorMapping[i] as keyof RGB] = (255 - lastDigits[i]);
+        result[colorMapping[i] as keyof RGB] = (255 - lastDigits[i]);
     }
 
     return rgbToHex(result.red, result.green, result.blue);
-   };
+ };
+
+export const blackAlgorithm = (rgb: RGB): string => { 
+    let colorMapping = ['red', 'green', 'blue'];
+    colorMapping.sort((a, b) => rgb[b] - rgb[a]);
+    let result: RGB = {red: 0, green: 0, blue: 0};
+
+    for (let i = 0; i < colorMapping.length; i++) {
+      let color = colorMapping[i];
+      let valueStr = rgb[color].toString();
+
+      // If it's the first color (the largest), take the first two digits
+      if (i === 0) {
+        result[color as keyof RGB] = Number(valueStr.slice(0, 2));
+      }
+      // Otherwise, take only the first digit
+      else {
+        result[color as keyof RGB] = Number(valueStr.slice(0, 1));
+      }
+    }
+
+    return rgbToHex(result.red, result.green, result.blue);
+ };
