82a316aa1caffd248cf4b41b427b847a1bced657
ellenuttley <ellenuttley@gmail.com>
2024-03-31 22:04:06 +0100

    created color palette client side component

--- src/components/ColourPalette.tsx
+++ src/components/ColourPalette.tsx
@@ -1,10 +1,29 @@
 'use client'
 
 import React from "react";
+import { useState, ChangeEvent } from 'react';
+import { hexToRgb } from "@/utils/colorAlgorithms";
+
+type RGB = {
+  red: number;
+  green: number;
+  blue: number;
+  [key: string]: number;
+};
 
 function ColorPalette() {
+    const [primaryColor, setPrimary] = useState<string>('#DD6031');
+
+    const handleColorChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setPrimary(e.target.value);
+        let rgb = hexToRgb(e.target.value);
+        return rgb;
+     };
+
     return (
-        <></>
+    <div className="flex flex-col space-x-2">
+        <input type="color" value={primaryColor} onChange={handleColorChange} className="w-24 h-24 p-2 border border-gray-300"/>
+    </div>
     );
 }
 
