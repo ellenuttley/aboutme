04187121831355cc7e01b649d9bd9227181ec66d
ellenuttley <ellenuttley@gmail.com>
2024-03-31 21:38:51 +0100

    white algorithm added

--- src/utils/colorAlgorithms.ts
+++ src/utils/colorAlgorithms.ts
@@ -18,3 +18,21 @@ export const hexToRgb = (hex: string): RGB => {
  export const rgbToHex = (r: number, g: number, b: number): string => { 
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   };
+
+  export const whiteAlgorithm = (rgb: RGB): string => { 
+    let lastDigits = [rgb.red, rgb.green, rgb.blue];
+    for (let i = 0; i < lastDigits.length; i++) {
+      lastDigits[i] = (lastDigits[i] % 10);
+    }
+    lastDigits.sort((a, b) => a - b);
+
+    let colorMapping = ['red', 'green', 'blue'];
+    colorMapping.sort((a, b) => rgb[b] - rgb[a]);
+
+    const result: RGB = {red: 0, green: 0, blue: 0};
+    for (let i = 0; i < colorMapping.length; i++) {
+      result[colorMapping[i] as keyof RGB] = (255 - lastDigits[i]);
+    }
+
+    return rgbToHex(result.red, result.green, result.blue);
+   };
