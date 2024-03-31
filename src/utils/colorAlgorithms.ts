3ea88f279f895012b35360ff219fd7d2acca1c15
ellenuttley <ellenuttley@gmail.com>
2024-03-31 21:34:50 +0100

    added function to convert hex colour codes to rgb, to then be used in the algorithms

--- src/utils/colorAlgorithms.ts
+++ src/utils/colorAlgorithms.ts
@@ -6,3 +6,11 @@ type RGB = {
   blue: number;
   [key: string]: number;
 };
+
+export const hexToRgb = (hex: string): RGB => { 
+    let red = parseInt(hex.slice(1, 3), 16),
+        green = parseInt(hex.slice(3, 5), 16),
+        blue = parseInt(hex.slice(5, 7), 16);
+
+    return { red, green, blue };
+ };
