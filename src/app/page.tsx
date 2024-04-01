c1ad637ec0d1dfdd235384b6a329705e98873553
ellenuttley <ellenuttley@gmail.com>
2024-04-01 23:46:14 +0100

    added first paragraphs! hover text working great

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import ColorPalette from "@/components/ColourPalette";
-
+import HoverText from "@/components/HoverText";
 export default function Home() {
+	let ott = "over the top ";
+	let impressive = "impressive";
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className=" font-calistoga"> Hello</h1>
-			<ColorPalette />
+		<main className="flex min-h-screen flex-col items-center p-24 *:mt-4 text-tBlack">
+			<h1 className="font-calistoga text-2xl">
+				Greetings, Tailwind hiring personnel
+			</h1>
+			<h1 className="font-calistoga text-2xl gap-4 ">
+				Get ready for an extremely...
+				<>
+					<HoverText />
+				</>
+				<br />
+				application for the role of Design Engineer
+			</h1>
+			{/* <ColorPalette /> */}
 		</main>
 	);
 }
