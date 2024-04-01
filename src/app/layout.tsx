ec55e1ee4b2f2d5c76332f572f09e44e51fde044
ellenuttley <ellenuttley@gmail.com>
2024-04-01 21:52:44 +0100

    added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,23 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Calistoga, Lexend } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const calistoga = Calistoga({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-calistoga',
+  weight: "400",
+  preload: true
+})
+
+const lexend = Lexend({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-lexend',
+  preload: true
+})
+ 
+ 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${calistoga.variable} ${lexend.variable}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import ColorPalette from "@/components/ColourPalette";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <><ColorPalette /></>
+      <h1 className=" font-calistoga"> Hello</h1>
+      <ColorPalette />
     </main>
   );
 }
