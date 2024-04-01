26e7701c35bde67e74f3e16f585856de2969d8f0
ellenuttley <ellenuttley@gmail.com>
2024-04-01 22:39:03 +0100

    used prettier for linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import ColorPalette from "@/components/ColourPalette";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className=" font-calistoga"> Hello</h1>
-      <ColorPalette />
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<h1 className=" font-calistoga"> Hello</h1>
+			<ColorPalette />
+		</main>
+	);
 }
