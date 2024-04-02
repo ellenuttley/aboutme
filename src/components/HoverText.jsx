1bcb826c45ce3f1c508c82d145e993f90028bf7d
ellenuttley <ellenuttley@gmail.com>
2024-04-02 13:13:28 +0100

    on hover effects for the homepage

--- src/components/HoverText.jsx
+++ src/components/HoverText.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 
-const HoverText = () => {
-	return (
-		<div className="group relative text-tWhite inline-block cursor-pointer before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-tSecondary ">
-			<p className="relative inline-block group-hover:hidden">over the top</p>
-			<p className="opacity-0 hidden relative group-hover:inline-block  group-hover:opacity-100">
-				impressive
-			</p>
-		</div>
-	);
+export const HoverTextImpressive = () => {
+  return (
+    <div className="*:whiteHeader group relative inline-block cursor-pointer leading-normal before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-tPrimary ">
+      <p className="whiteHeader relative inline-block whitespace-break-spaces group-hover:hidden">
+        over the top
+      </p>
+      <p className="whiteHeader relative hidden whitespace-break-spaces opacity-0 group-hover:inline-block  group-hover:opacity-100">
+        impressive
+      </p>
+    </div>
+  );
 };
 
-export default HoverText;
+export const HoverTextAutism = () => {
+  return (
+    //     <div className="*:whiteHeader group static  flex cursor-pointer flex-col whitespace-break-spaces leading-normal before:absolute before:-inset-1 before:block group-hover:absolute">
+    //       <span className="page">
+    //         I have
+    //         <p className=" relative inline-block whitespace-break-spaces text-tSecondary">
+    //           {" "}
+    //           Autism
+    //         </p>
+    //       </span>
+    //       <p className="whiteHeader relative hidden text-right text-base opacity-0 group-hover:inline-block group-hover:flex-grow group-hover:opacity-100">
+    //         {" "}
+    //         ... and that's a good thing
+    //       </p>
+    //     </div>
+    //   );
+    <></>
+  );
+};
