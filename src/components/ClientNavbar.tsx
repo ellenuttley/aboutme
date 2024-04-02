85e7b48a79b9bc5774767e377cc894cfd66943c6
ellenuttley <ellenuttley@gmail.com>
2024-04-02 22:36:35 +0100

    not sure if this is actually doing anything anymore

--- src/components/ClientNavbar.tsx
+++ src/components/ClientNavbar.tsx
@@ -3,18 +3,20 @@ import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 
 export default function ClientNavbar() {
-  const [darkMode, setDarkMode] = useState(false);
+  // const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    const prefersDarkMode =
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
-    setDarkMode(prefersDarkMode);
-  }, []);
+  // useEffect(() => {
+  //   const prefersDarkMode =
+  //     window.matchMedia &&
+  //     window.matchMedia("(prefers-color-scheme: dark)").matches;
+  //   setDarkMode(prefersDarkMode);
+  // }, []);
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+  // const toggleDarkMode = () => {
+  //   setDarkMode(!darkMode);
+  // };
 
-  return <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />;
+  // return <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />;
+  
+  return <Navbar />;
 }
