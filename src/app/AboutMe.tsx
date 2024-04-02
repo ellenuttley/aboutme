c850de89ec359fac07360067e3a4a9c3bdc544dd
ellenuttley <ellenuttley@gmail.com>
2024-04-02 12:34:19 +0100

    removed no longer needed code from AboutMe

--- src/app/AboutMe.tsx
+++ src/app/AboutMe.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import { HoverTextAutism } from "@/components/HoverText";
+import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
+import QuoteBox from "@/components/QuoteBox";
+import AutismAccordion from "@/components/AutismAccordion";
 import ColorPalette from "@/components/ColourPalette";
 
 const AboutMe = () => {
   return (
-    <div className="grid grid-cols-2 gap-2 gap-y-5">
+    <div className="grid grid-cols-3 gap-2 gap-y-5">
       <div className="col-span-1">
         <figure>
           <Image
@@ -15,66 +18,25 @@ const AboutMe = () => {
             height={500}
             alt="Portrait picture of Ellen"
           />
-          <figcaption className="box text-center text-sm mt-1 ">
+          <figcaption className="box mt-1 p-1 text-center font-lexend text-sm">
             I have never looked this good in real life
           </figcaption>
         </figure>
       </div>
-      <div className="col-span-1 text-left page ">
+      <div className="box col-span-2 text-left ">
         <h1>I am Ellen</h1>
-        <div className="flex *:space-y-2">
-          <ul>
-            <li>
-              I started my development journey when I was 13, by designing and
-              creating MySpace profiles
-            </li>
-            <li>I love design, and I love development</li>
-          </ul>
-        </div>
-      </div>
-      <div className="col-span-2 box inset-2">
-        <h2>
-          <span className="relative inline-block space-x-2">I am</span>
-          <HoverTextAutism />
-          <br />
-        </h2>
         <div className="flex text-left *:space-y-2">
           <ul>
-            <li>I am an extremely fast learner, and fanatically self taught</li>
             <li>
-              I have a keen eye for predicting user behaviours, and I love to
-              use to satisfy and simplify the user story.
+              I started coding when I was 13, by designing and creating MySpace
+              profiles
             </li>
-            <li>
-              I can mentally trace a bug back to its root, and a feature forward
-              to its impact
-            </li>
-            <li>
-              I can sense whether an element is off centre by even 1px...and I
-              am genetically incapable of leaving it that way
-            </li>
-            <li>
-              I am a creative problem solver, and this has been an asset in
-              every team I have been a part of
-            </li>
-            <li>I can recognize patterns in design blocks, hex codes, data</li>
+            <li>But I did not know what HTML was until I was 30</li>
           </ul>
         </div>
       </div>
+      <AutismAccordion />
 
-
-      <div className="col-span-1">
-        <ColorPalette />
-      </div>
-
-      <div className="col-span-2">
-        <h1 className="blackHeader">You say you want someone who can...</h1>
-        <div className="flex *:space-y-2"></div>
-      </div>
-
-
-      <div className="col-span-2"></div>
-      <div className="col-span-2"></div>
     </div>
   );
 };
