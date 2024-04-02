f652084459445c244e2147871aacaebabccb9f3d
ellenuttley <ellenuttley@gmail.com>
2024-04-02 22:37:47 +0100

    adjusted the styling for the quotebox component

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -4,12 +4,12 @@ import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 const QuoteBox = ({ quote }) => {
   return (
     <div className="col-span-3">
-      <div className="quotebox flex flex-row justify-around rounded-e-full rounded-s-full">
-        <RiDoubleQuotesL className="border-tGrey shadow-tGrey/80 rounded-full border-2 bg-tSecondary p-0.5 text-4xl text-tBlack shadow-inner" />
-        <p className="flex flex-grow place-items-center justify-evenly text-center font-calistoga text-sm max-md:text-base lg:text-xl">
+      <div className="quotebox flex flex-row place-items-center  justify-evenly rounded-e-full rounded-s-full ">
+        <RiDoubleQuotesL className=" rounded-full border-2 border-tGrey bg-tSecondary p-0.5 text-4xl text-tBlack shadow-inner shadow-tGrey/80" />
+        <p className="flex flex-grow px-1 place-items-center justify-evenly text-center font-calistoga text-sm max-md:text-base lg:text-xl">
           {quote}
         </p>
-        <RiDoubleQuotesR className="border-tGrey shadow-tGrey/80 rounded-full border-2 bg-tSecondary p-0.5 text-4xl text-tBlack shadow-inner" />
+        <RiDoubleQuotesR className="rounded-full border-2 border-tGrey bg-tSecondary p-0.5 text-4xl text-tBlack shadow-inner shadow-tGrey/80" />
       </div>
     </div>
   );
