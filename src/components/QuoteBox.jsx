c6c404115744e84eb7bc032d69f83ef55f0b3f41
ellenuttley <ellenuttley@gmail.com>
2024-04-03 10:54:18 +0100

    linted

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -5,11 +5,9 @@ const QuoteBox = ({ quote }) => {
   return (
     <div className="col-span-3">
       <div className="quotebox-wrapper">
-        <RiDoubleQuotesL className="quotebox-icon" />
-        <p className="quotebox">
-          {quote}
-        </p>
-        <RiDoubleQuotesR className="quotebox-icon" />
+        <RiDoubleQuotesL className="quotebox-icon border-l-2" />
+        <p className="quotebox">{quote}</p>
+        <RiDoubleQuotesR className="quotebox-icon border-r-2" />
       </div>
     </div>
   );
