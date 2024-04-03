5d72d95f4dfc622f01e2a5a7ca2ff448f232ac53
ellenuttley <ellenuttley@gmail.com>
2024-04-03 05:21:04 +0100

    added quotebox custom styles

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -4,12 +4,12 @@ import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 const QuoteBox = ({ quote }) => {
   return (
     <div className="col-span-3">
-      <div className="quotebox flex flex-row place-items-center  justify-evenly rounded-e-full rounded-s-full ">
-        <RiDoubleQuotesL className=" rounded-full border-2 border-tGrey bg-tSecondary p-0.5 text-4xl text-tBlack shadow-inner shadow-tGrey/80" />
-        <p className="flex flex-grow px-1 place-items-center justify-evenly text-center font-calistoga text-sm max-md:text-base lg:text-xl">
+      <div className="quotebox-wrapper">
+        <RiDoubleQuotesL className="quotebox-icon" />
+        <p className="quotebox">
           {quote}
         </p>
-        <RiDoubleQuotesR className="rounded-full border-2 border-tGrey bg-tSecondary p-0.5 text-4xl text-tBlack shadow-inner shadow-tGrey/80" />
+        <RiDoubleQuotesR className="quotebox-icon" />
       </div>
     </div>
   );
