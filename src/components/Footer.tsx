6bdbc0005a686f8b4cc43132414dd5b937b7216d
ellenuttley <ellenuttley@gmail.com>
2024-04-03 03:54:47 +0100

    fixed clipboard notification

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,38 +10,34 @@ const Footer = () => {
  const copyToClipboard = () => {
    navigator.clipboard.writeText(email);
    setShowNotification(true);
-   setTimeout(() => setShowNotification(false), 2000); // Hide notification after 2 seconds
+   setTimeout(() => setShowNotification(false), 2000); 
  };
 
   return (
-    <footer className="group text-tBlack font-lexend fixed bottom-1 left-1 right-1 z-10 flex w-full flex-row justify-evenly rounded-md border-2 border-solid bg-tWhite pb-2 pt-2 opacity-10 shadow-md shadow-tPrimary hover:opacity-100 dark:border-tPrimary dark:bg-tPrimary dark:shadow-tSecondary">
+    <footer className="s group fixed bottom-1 left-1 right-1 z-10 flex w-full translate-y-full transform flex-row justify-evenly rounded-md border-2 border-solid bg-tWhite pb-2 pt-2 font-lexend text-tBlack opacity-10 shadow-md shadow-tPrimary transition-transform duration-300 hover:opacity-100 hover:translate-y-0 dark:border-tPrimary dark:bg-tPrimary dark:shadow-tSecondary">
       <a
-        href="https://github.com/yourusername"
+        href="https://github.com/ellenuttley/aboutme"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center space-x-2"
+        className="sidebar-icon"
       >
-        <FiGithub size="22" className="sidebar-icon" />
-        <span>View the code for this site</span>
+        <FiGithub size="22" className="md:mr-2" />
+        <span className="hidden md:inline">View the code for this site</span>
       </a>
       <a
         href="https://www.linkedin.com/in/ellenuttley"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center space-x-2"
+        className="sidebar-icon"
       >
-        <FaLinkedinIn size="22" className="sidebar-icon" />
-        <span>Visit my LinkedIn</span>
+        <FaLinkedinIn size="22" className="md:mr-2" />
+        <span className="hidden md:inline">Visit my LinkedIn</span>
       </a>
-      <div className="flex cursor-pointer items-center space-x-2">
-        <FiClipboard
-          size="22"
-          onClick={copyToClipboard}
-          className="sidebar-icon"
-        />
-                <a>{email}</a>
+      <div className="sidebar-icon" onClick={copyToClipboard}>
+        <FiClipboard size="22" className="mr-2" />
+        <a className="">{email}</a>
         {showNotification && (
-          <div className="absolute translate-x-10 px-2 py-1 bg-tPrimary text-tWhite text-xs rounded shadow-md">
+          <div className="absolute translate-x-10 rounded bg-tPrimary px-2 py-1 text-xs text-tWhite shadow-md">
             Email copied to clipboard!
           </div>
         )}
