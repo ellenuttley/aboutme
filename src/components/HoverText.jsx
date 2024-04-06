4331330ca3f2230c92c78a2ef71f5e8fbbd1e384
ellenuttley <ellenuttley@gmail.com>
2024-04-06 02:54:54 +0100

    edited text styling

--- src/components/HoverText.jsx
+++ src/components/HoverText.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export const HoverTextImpressive = () => {
   return (
     <div className="*:whiteHeader group relative inline-block cursor-pointer leading-normal before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-tPrimary ">
-      <p className="whiteHeader relative inline-block whitespace-break-spaces group-hover:hidden">
+      <h2 className="whiteHeader relative inline-block whitespace-break-spaces group-hover:hidden">
         over the top
-      </p>
-      <p className="whiteHeader relative hidden whitespace-break-spaces opacity-0 group-hover:inline-block  group-hover:opacity-100">
+      </h2>
+      <h2 className="whiteHeader relative hidden whitespace-break-spaces opacity-0 group-hover:inline-block  group-hover:opacity-100">
         impressive
-      </p>
+      </h2>
     </div>
   );
 };
@@ -16,14 +16,14 @@ export const HoverTextImpressive = () => {
 export const HoverTextAutism = () => {
   return (
     <div className="*:whiteHeader  cursor-pointeroverflow-x-visible group relative inline-block leading-normal before:absolute before:block ">
-      <p className="relative inline-block  whitespace-break-spaces text-tPrimary ">
+      <span className=" relative inline-block whitespace-break-spaces text-2xl leading-loose text-tPrimary transition-transform  md:text-4xl lg:text-5xl ">
         {" "}
         Autistic
-      </p>
-      <p className="relative hidden whitespace-break-spaces  text-tTransition opacity-0 group-hover:inline-block group-hover:opacity-100">
+      </span>
+      <span className="lg:text-5xl hidden relative whitespace-break-spaces text-2xl leading-loose  text-tTransition opacity-0  transition-transform group-hover:inline-block group-hover:opacity-100 md:text-4xl">
         {" "}
         and that is a good thing
-      </p>
+      </span>
     </div>
   );
 };
