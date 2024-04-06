9417e63d72638931e4bc6973bd08a8105802b145
ellenuttley <ellenuttley@gmail.com>
2024-04-06 02:56:04 +0100

    blank code component

--- src/app/MyCode.tsx
+++ src/app/MyCode.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import ReviewBox from "@/components/ReviewBox";
-import ProjectSwiper from "@/components/ProjectSwiper";
+import MyProjects from "@/components/MyProjects";
 
 const MyCode = () => {
   return (
     <div>
-      <ReviewBox
-        count={5}
-        review="Yours is the only training that doesn’t make me want to jump out of the window" 
-        name="Actual quote from a former teammate" />
-      <ProjectSwiper />
+      <MyProjects />
     </div>
   );
 };
